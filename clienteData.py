6f9596c5a178f6f2427e8eac1678cd8b198251cf
Eduardo Martins Lopes <edumlopes@gmail.com>
2010-01-05 22:06:32 -0200

    Iniciando o controle de versao dos aquivos do grid-qnint

---
+++ clienteData.py
@@ -0,0 +1,63 @@
+#-*- coding:utf-8 -*-
+
+'''Este modulo contem as funcoes usadas para mapear a carga do cliente, hostname e ip. Nenhuma aceita variaveis de entrada '''
+
+def carga():
+    
+    '''funcao usada para levantar a carga media em 5min da cpu '''
+    
+    import commands
+    import os
+    import clienteErros
+    
+    try:
+        output = os.getloadavg()
+        carga = output[2]
+        return carga
+    except:
+        mensagem = 'impossivel obter a carga de uso em seu equipamento. Ele podera ser incluido no Grid-QNInt ate resolvermos esse problema. \n'
+        sys.stderr.write(mensagem)
+        clienteErros.registrar('clienteData.carga', mensagem) #registra o erro no log do programa
+
+    
+def hostname():
+    '''Esta funcao detecta o hostname do pc'''
+    #a
+    
+
+def pegarIP():
+    
+    '''Esta funcao detecta qual e o verdadeiro endereco ip com o qual a maquina acessa a internet atraves do que o site www.qualmeuip.net retorna ao acessa-la atraves do python'''
+     
+    import urllib #biblioteca padrao para manipular url's
+    import sys
+    import clienteErros
+    
+    try:
+        conexao = urllib.urlopen("http://www.qualmeuip.net") #acessando a pagina
+        conteudo = conexao.readlines() #copiando todo o conteudo da pagina
+        linha = conteudo[66] #linha 66
+        #entre as marcacoes <strong> e </strong>
+        inicio = linha.find('g>') 
+        fim = linha.find('</') 
+        inicio = inicio + 2 #acerto dos caracteres
+        ip = linha[inicio:fim]
+
+        return ip
+
+    
+    except:
+        #escreve a mensagem de erro na saida de erro padrao
+        mensagem = 'sem conexao com a internet. Seu equipamento nao poder ser incluido no GRID-QNInt ate resolver esse problema. \n'
+        sys.stderr.write(mensagem)
+        #registra o erro no log
+        clienteErros.registrar('clienteData.pegarIP', mensagem)
+        
+    
+    
+
+    
+        
+        
+    
+    
---
+++ clientePastas.py
@@ -0,0 +1,64 @@
+#-*- coding: latin-1 -*-
+
+'''Este módulo é responsável pela preparação do cliente para operar como parte do sistema de cálculo da SBQInt'''
+
+
+def listar():
+    
+    '''funcao usada apenas para armazenar os diretorios em uma lista'''
+
+    import os
+    import sys
+
+    nome = os.getlogin() 
+    caminhoraiz = '/home/'+nome+'/.qnint'
+    logs = caminhoraiz+'/logs'
+    sistema = caminhoraiz+'/bin'
+    
+    diretorios = [nome, caminhoraiz, logs, sistema]
+    
+    return diretorios
+    
+
+def criar():
+    
+    import os
+    import sys
+
+    diretorios = listar()
+    caminhoraiz = diretorios[1]
+    logs = diretorios[2]
+    sistema = diretorios[3]
+
+    try:
+        os.mkdir(caminhoraiz)
+    except:
+        mensagem = 'Possivelmente ja existe a pasta %s e nao ha necessidade de apagar seu conteudo neste momento \n' %(caminhoraiz)
+        sys.stderr.write(mensagem)
+    
+    try:        
+        os.mkdir(logs)
+        os.mkdir(sistema)
+        
+    except:
+        mensagem = 'Nao foi possivel criar as sub-pastas necessarias, por favor remova as pasts %s e %s \n' %(logs, systema)
+        sys.stderr.write(mensagem)
+
+def remover():
+    
+    import commands
+    import sys
+    
+    diretorios = listar()
+    comando = 'rm -r ' + diretorios[1]
+    
+    try: 
+        commands.getoutput(comando)
+        
+    except:
+        
+        sys.stderr.write('Remova manualmente os seguintes diretorios: \n')
+        sys.stderr.write(diretorios)
+        
+        
+    
